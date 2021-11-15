4d60f8fd3dfc7eaee4de6b6f3d3aaebb3058893e
LABORDE-Yohann <68321361+LABORDE-Yohann@users.noreply.github.com>
2021-11-15 15:29:06 +0100

    15/11/2021 15:28

--- game/main.cpp
+++ game/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    cout << "Bienvenue juste prix" << endl;
+
+    int proposition{0};
+
+    srand((unsigned int)time(0));
+
+    for (int i = 0; i < 10; i++)
+        {
+        int aDeviner = rand()%10000 +1;
+
+        do
+        {
+            cin >> proposition;
+
+            //afficher la variable
+
+            cout << "vous avez dit ";
+            cout << proposition << endl;
+
+
+            if (proposition < 0) {
+
+            }else if (aDeviner != proposition) {
+                if (aDeviner > proposition) {
+                cout << "c'est plus" << endl;
+                }else; {
+                cout << "c'est moins" << endl;
+                }
+            }
+        }
+        while (aDeviner != proposition || proposition > 0);
+
+        if (aDeviner == proposition) {
+            cout << "félicitation t'as gagné petit battard !!!";
+            cout << "la valeur était bien";
+            cout << aDeviner << endl;
+        }else; {
+            cout <<"merci de donnez une valeur positive";
+        }
+    }
 }
